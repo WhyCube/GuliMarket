22dfac9460f4038e6e6e32f8745bfa0687092bbb
WHYPD <214770624@qq.com>
2020-12-04 15:54:09 +0800

    添加公共依赖，使用renrenfast后端管理模板

--- mail-order/src/main/java/com/why/mailorder/MailOrderApplication.java
+++ mail-order/src/main/java/com/why/mailorder/MailOrderApplication.java
@@ -1,8 +1,12 @@
 package com.why.mailorder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.why.mailorder.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MailOrderApplication {
 
--- mail-order/src/test/java/com/why/mailorder/MailOrderApplicationTests.java
+++ mail-order/src/test/java/com/why/mailorder/MailOrderApplicationTests.java
@@ -1,13 +1,22 @@
 package com.why.mailorder;
 
+import com.why.mailorder.entity.OrderEntity;
+import com.why.mailorder.service.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MailOrderApplicationTests {
 
+    @Autowired
+    OrderService orderService;
     @Test
     void contextLoads() {
+        OrderEntity orderEntity=new OrderEntity();
+        orderEntity.setBillContent("测试！");
+        orderService.save(orderEntity);
+        System.out.println("成功");
     }
 
 }
